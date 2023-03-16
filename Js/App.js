02de6d53996fad9fc6a7e16741d90017fb36702d
JonathanBergfastNTIJohanneberg <jonathan.bergfast@elev.ga.ntig.se>
2023-03-16 15:11:32 +0100

    Fixasde mina articles vid homepage , behöver
    Det som är kvar att göra i min homepage, är att göra så att mina articles går att använda för mindre skärmar

--- Js/App.js
+++ Js/App.js
@@ -1,16 +1,14 @@
+const likeButtons = document.querySelectorAll('.like-button');
 
-// tar id burger från html koden, skapar en konstant variabel vid namn burger
-const burger = document.getElementById('burger')
-
-//gör en ny konstant variabel vid namn nav, tar nav_links id
-const nav = document.getElementById('nav_links')
-
-
-//skapat en funktion som går in i våra konstanter ovan, och byter ut deras id om exempelvis burger_toggle finns med eller låter den vara ifall den inte gör det
-function toggleMenu() {
-    burger.classList.toggle('burger_toggle')
-    nav.classList.toggle('nav_active')
-}
-
-//Hemsidan "Lyssnar" på vad som görs på hemsidan, vid detta fall när man trycker på våran hamburgar meny
-burger.addEventListener('click', toggleMenu)
+likeButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const buttonText = event.target.textContent;
+    if (buttonText === 'Like') {
+      event.target.textContent = 'Liked';
+      event.target.style.backgroundColor = '#28a745';
+    } else {
+      event.target.textContent = 'Like';
+      event.target.style.backgroundColor = '#007bff';
+    }
+  });
+});
