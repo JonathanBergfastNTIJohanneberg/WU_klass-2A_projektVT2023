33ce5017990c783d09c73249f3f9a3051a466840
JonathanBergfastNTIJohanneberg <jonathan.bergfast@elev.ga.ntig.se>
2023-06-02 23:31:53 +0200

    Inlämning av komplettering, kunde inte fixa javascript koden i order filen så att den fungerande med resten av javascript koden
    Så jag lade bara in den i en script tag, hoppas att det är okej

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const menuBtn = document.querySelector('.menu-btn');
 const navLinks = document.querySelector('.nav-links');
 const likeButtons = document.querySelectorAll('.like-button');
 const filterButtons = document.querySelectorAll('.filter_button');
+const priceRangeInput = document.getElementById('price-range');
+const articles = document.querySelectorAll('.box');
 
 filterButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -39,3 +41,24 @@ menuBtn.addEventListener('click', function () {
     this.classList.toggle('active');
     navLinks.classList.toggle('active');
 });
+
+
+priceRangeInput.addEventListener('input', updateArticleVisibility);
+
+
+function updateArticleVisibility() {
+  const priceRange = parseInt(priceRangeInput.value);
+  
+  
+  articles.forEach((article) => {
+    const priceElement = article.querySelector('.price');
+    const price = parseInt(priceElement.textContent.substring(1));
+    
+   
+    if (price <= priceRange || priceElement.textContent === 'Free') {
+      article.style.display = 'block';
+    } else {
+      article.style.display = 'none';
+    }
+  });
+}
